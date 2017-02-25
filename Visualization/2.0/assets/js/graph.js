25f4dfaa0863218596623dde6d7b216545091dd7
Clarisse Poblete <Clarisse@Clarisses-MacBook-Pro.local>
2017-02-25 14:58:43 +0800

    Took into account values for distances and added hover for name

--- Visualization/2.0/assets/js/graph.js
+++ Visualization/2.0/assets/js/graph.js
@@ -13,7 +13,9 @@ window.onload=function(){
     //Set up the force layout
     force = d3.layout.force()
         .charge(-120)
-        .linkDistance(100)
+        .linkDistance(function(d) { 
+            return(1/(d.value+1) * 300); 
+        })
         .size([width, height]);
 
     //Append a SVG to the body of the html page. Assign this SVG as an object to svg
@@ -44,14 +46,28 @@ function readGraphJSON(filename){
         //Do the same with the circles for the nodes - no 
         var node = svg.selectAll(".node")
             .data(graph.nodes)
-            .enter().append("circle")
+            .enter().append("g")
             .attr("class", "node")
+            .call(force.drag);
+
+        node.append("circle")
             .attr("r", 8)
             .style("fill", function (d) {
             return color(d.group);
-        })
-            .call(force.drag);
+        });
 
+        node.append("text")
+              .attr("dx", 10)
+              .attr("dy", ".35em")
+              .attr("font-family", "Arial")
+              .attr("font-size", "15px")
+              .text(function(d) { return d.name })
+              .style("stroke", "rgb(50,50,50)")
+              .style("opacity", "0")
+              .style("z-index", 2000);
+
+        node.on("mouseover", mouseover)
+            .on("mouseout", mouseout)
 
         //Now we are giving the SVGs co-ordinates - the force layout is generating the co-ordinates which this code is using to update the attributes of the SVG elements
         force.on("tick", function () {
@@ -67,13 +83,28 @@ function readGraphJSON(filename){
                 .attr("y2", function (d) {
                 return d.target.y;
             });
-
-            node.attr("cx", function (d) {
+            d3.selectAll("circle").attr("cx", function (d) {
                 return d.x;
             })
                 .attr("cy", function (d) {
                 return d.y;
             });
+            d3.selectAll("text").attr("x", function (d) {
+                return d.x;
+            })
+                .attr("y", function (d) {
+                return d.y;
+            });
         });
     });
+}
+
+function mouseover() {
+    d3.select(this).select("text").transition()
+      .style("opacity", "1")
+}
+
+function mouseout() {
+    d3.select(this).select("text").transition()
+      .style("opacity", "0")
 }
