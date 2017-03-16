cb11b3244d4e47ba1f8d7d9c5c4a3e68a232a6dd
Clarisse Poblete <Clarisse@Clarisses-MacBook-Pro.local>
2017-03-16 10:04:36 +0800

    Fixed some problems with K-means (hopefully)

--- algorithms.py
+++ algorithms.py
@@ -11,91 +11,107 @@ class KMeans(Algorithm):
 	def run(self, users):
 		numClusters = self.k # For now this is hardcoded
 
-		userIds = list(users.keys()) # Create list of user ideas
-		indices = random.sample(range(0, len(users)), numClusters) # Randomize centroids for each cluster (returns indices in userIds list)
-		centroids = [] # Initialize array of centroids
-		userClusters = {} # Initialize dictionary of user clusters (maps user ids to their clusters)
-		prevUserClusters = {} # Initialize dictionary of previous user clusters (copy of previous iteration's user clusters)
-		clusters = {} # Maps cluster centroids to dictionaries of users in the cluster
+		# If there are less users than clusters that need to be formed
+		if len(users) < numClusters:
+			communities = []
 
-		iterationCount = 1 # Initialize iteration count to 1
+			# Form a community for each user
+			for key, value in users.items():
+				c = Community()
+				c.addUser(value)
+				communities.append(c)
 
-		# Add randomly selected centroids to centroids array
-		for i in indices:
-			centroids.append(users[userIds[i]])
-		
-		# Assign users to clusters with closest centroids until clusters don't change
-		while True:
-			clusters = {}
-			prevUserClusters = userClusters.copy() # Copy current set of user clusters to previous set of user clusters
+			# Return the generated communities
+			return communities
 
-			# Empty clusters
-			for c in centroids:
-				clusters.update({c.id: {}})
+		# If there are at least as many users as clusters that need to be formed
+		else:
+			userIds = list(users.keys()) # Create list of user ideas
+			indices = random.sample(range(0, len(users)), numClusters) # Randomize centroids for each cluster (returns indices in userIds list)
+			centroids = [] # Initialize array of centroids
+			userClusters = {} # Initialize dictionary of user clusters (maps user ids to their clusters)
+			prevUserClusters = {} # Initialize dictionary of previous user clusters (copy of previous iteration's user clusters)
+			clusters = {} # Maps cluster centroids to dictionaries of users in the cluster
 
-			# Assign users to more similar centroids
-			for u in users.values():
-				closestCentroid = None
-				maxSimilarity = None
+			iterationCount = 1 # Initialize iteration count to 1
 
-				# Compare with every centroid
+			# Add randomly selected centroids to centroids array
+			for i in indices:
+				centroids.append(users[userIds[i]])
+			
+			# Assign users to clusters with closest centroids until clusters don't change
+			while True:
+				clusters = {}
+				prevUserClusters = userClusters.copy() # Copy current set of user clusters to previous set of user clusters
+
+				# Empty clusters
 				for c in centroids:
-					currSimilarity = self.parameter.similarity(c, u) # Check similarity between current centroid and current user
-					# print(c.id, u.id, currSimilarity)
+					clusters.update({c.id: {}})
 
-					# Set closest centroid of user if so far current centroid is the most similar
-					if closestCentroid is None or currSimilarity > maxSimilarity:
-						closestCentroid = c.id
-						maxSimilarity = currSimilarity
+				# Assign users to more similar centroids
+				for u in users.values():
+					closestCentroid = None
+					maxSimilarity = None
 
-				# Once closest centroid to user is found, update lists
-				clusters[closestCentroid].update({u.id: u})
-				userClusters[u.id] = closestCentroid
-				# print("closestCentroid:", closestCentroid)
+					# Compare with every centroid
+					for c in centroids:
+						currSimilarity = self.parameter.similarity(c, u) # Check similarity between current centroid and current user
+						# print(c.id, u.id, currSimilarity)
 
-			end = True
+						# Set closest centroid of user if so far current centroid is the most similar
+						if closestCentroid is None or currSimilarity > maxSimilarity:
+							closestCentroid = c.id
+							maxSimilarity = currSimilarity
 
-			# Check if user clusters are the same as in the previous iteration
-			for uc in userClusters.keys():
-				if uc in prevUserClusters:
-					if not prevUserClusters[uc] == userClusters[uc]: # If current user's cluster was different in the previous iteration, clusters aren't final
+					# Once closest centroid to user is found, update lists
+					clusters[closestCentroid].update({u.id: u})
+					userClusters[u.id] = closestCentroid
+					# print("closestCentroid:", closestCentroid)
+
+				end = True
+
+				# Check if user clusters are the same as in the previous iteration
+				for uc in userClusters.keys():
+					if uc in prevUserClusters:
+						if not prevUserClusters[uc] == userClusters[uc]: # If current user's cluster was different in the previous iteration, clusters aren't final
+							end = False
+					else: # If the user doesn't even exist in the prevUserClusters (NOTE: Not actually sure how this would happen), clusters aren't final
 						end = False
-				else: # If the user doesn't even exist in the prevUserClusters (NOTE: Not actually sure how this would happen), clusters aren't final
-					end = False
 
-			# print("userClusters:", userClusters)
-			# print("prevUserClusters:", prevUserClusters)
+				# print("userClusters:", userClusters)
+				# print("prevUserClusters:", prevUserClusters)
 
-			# Break out of loop if clusters are final
-			if end:
-				break
+				# Break out of loop if clusters are final
+				if end:
+					break
 
-			newCentroids = []
+				newCentroids = []
 
-			# Set new centroids for next iteration
-			for c in centroids:
-				averageUser = self.parameter.average(clusters[c.id]) # Generate the average user for the cluster of the given centroid
-				averageUser.id = c.id # Assign the ID of the generated user (so it doesn't repeat)
-				newCentroids.append(averageUser) # Add the generated user to the new list of centroids
+				# Set new centroids for next iteration
+				for c in centroids:
+					averageUser = self.parameter.average(clusters[c.id]) # Generate the average user for the cluster of the given centroid
+					averageUser.id = c.id # Assign the ID of the generated user (so it doesn't repeat)
+					newCentroids.append(averageUser) # Add the generated user to the new list of centroids
 
-			centroids = newCentroids # Set new centroids as current centroids
+				centroids = newCentroids # Set new centroids as current centroids
 
-			print("\nIteration:", iterationCount)
-			iterationCount += 1
+				print("\nIteration:", iterationCount)
+				iterationCount += 1
 
-		communities = []
+			communities = []
 
-		print("\nStopped at iteration #", iterationCount)
+			print("\nStopped at iteration #", iterationCount)
 
-		# Convert clusters to communities
-		for key, value in clusters.items():
-			c = Community();
-			for k,v in value.items():
-				c.addUser(v)
-			communities.append(c)
+			# Convert clusters to communities
+			for key, value in clusters.items():
+				c = Community()
+				for k,v in value.items():
+					c.addUser(v)
+				if c.len() > 0:
+					communities.append(c)
 
-		# Return the generated communities
-		return communities
+			# Return the generated communities
+			return communities
 
 class DivisiveHC(Algorithm):
 
